a07405bdd8c010c7dee2bd7deb06d5b3927278ca
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2022-10-14 02:09:30 +0600

    The solution updated with set

--- USACO.GUIDE/BRONZE/USACO_831.py
+++ USACO.GUIDE/BRONZE/USACO_831.py
@@ -53,7 +53,8 @@ def find_wins1(tmp_board, i, j):
 
     k = [tmp_board[0][0], tmp_board[1][1], tmp_board[2][2]]
     i_ = k.count(i)
-    j_ = k.count(j)
+    j_ = k.count(j) 78 lines (66 sloc) 1.93 KB
+
     if i_ + j_ == 3 and i_ > 0 and j_ > 0:
         return 1
     k = [tmp_board[0][2], tmp_board[1][1], tmp_board[2][0]]
@@ -76,3 +77,64 @@ for i,j in list(combinations(ascii_uppercase, 2)):
 
 ans += "\n"+str(team_wins)
 print(ans)
+
+
+
+#solution with set
+input = open('tttt.in', 'r').readline
+print = lambda x : open('tttt.out', 'w').write(str(x)+'\n')
+
+
+
+
+from collections import defaultdict
+
+
+arr = []
+
+for i in range(3):
+    arr.append(input().rstrip())
+
+# rows
+r = defaultdict(set)
+for i in range(3):
+    for j in range(3):
+        r[i].add(arr[i][j])
+
+# columns
+c = defaultdict(set)
+for i in range(3):
+    for j in range(3):
+        c[i].add(arr[j][i])
+
+# diagonals
+d = defaultdict(set)
+d[1].add(arr[0][0])
+d[1].add(arr[1][1])
+d[1].add(arr[2][2])
+
+d[2].add(arr[0][2])
+d[2].add(arr[1][1])
+d[2].add(arr[2][0])
+
+
+def sol(r, c, d, x):
+    wins1 = set()
+    for k in r:
+        if len(r[k]) == x:
+            wins1.add("".join(sorted(r[k])))
+    for k in c:
+        if len(c[k]) == x:
+            wins1.add("".join(sorted(c[k])))
+    for k in d:
+        if len(d[k]) == x:
+            wins1.add("".join(sorted(d[k])))
+    return len(wins1)
+
+
+# single wins
+a1 = sol(r, c, d, x=1)
+# dual wins
+a2 = sol(r, c, d, x=2)
+
+print(str(a1) +'\n' + str(a2))
