ee34f456f40e77b4ef0442d82328fe9f458b80ef
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2022-12-05 18:47:25 +0600

    Update USACO_965.py

--- USACO.GUIDE/BRONZE/USACO_965.py
+++ USACO.GUIDE/BRONZE/USACO_965.py
@@ -7,7 +7,6 @@ fout = open('lineup.out', 'w')
 n = int(input())
 d = defaultdict(list)
 
-    
 
 def check_valid(order: list, d):
     for u in d:
@@ -16,8 +15,6 @@ def check_valid(order: list, d):
             if not (v == order[min(7, i+1)] or v == order[max(0, i-1)]):
                 return False
     return True
-        
-
 
 
 for i in range(n):
@@ -26,11 +23,12 @@ for i in range(n):
     d[line[-1]].append(line[0])
 
 
-cows = ['Bessie', 'Buttercup', 'Belinda', 'Beatrice', 'Bella', 'Blue', 'Betsy', 'Sue']
+cows = ['Bessie', 'Buttercup', 'Belinda',
+        'Beatrice', 'Bella', 'Blue', 'Betsy', 'Sue']
 cows.sort()
 order_permutations = permutations(cows)
 
 for order in order_permutations:
     if check_valid(order, d):
         print(*order, sep="\n", file=fout)
-        break   
+        break
