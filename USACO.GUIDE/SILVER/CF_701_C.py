b90e030dbd6a4e286a45ce499bf081d4d91ff1d2
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2023-07-01 00:25:24 +0600

    Update CF_701_C.py

--- USACO.GUIDE/SILVER/CF_701_C.py
+++ USACO.GUIDE/SILVER/CF_701_C.py
@@ -1,65 +1,25 @@
 from collections import defaultdict
-
-
-def check(arr, c, i):
-    if c[arr[i]] - 1 > 0:
-        return True
-    return False
-
-def check_rev(arr, c, j):
-    if c[arr[j]] - 1 > 0:
-        return True
-    return False
-
 n = int(input())
 arr = list(input())
-
-count = defaultdict(int)
-
-for c in arr:
-    count[c] += 1
     
-i = 0
-j = n - 1
-# print(count)
-ans = []
-
-while True:
-    if check(arr, count, i):
-        count[arr[i]] -= 1
-        i += 1
-    else:
-        break
-while True:
-    if check_rev(arr, count, j):
-        count[arr[j]] -= 1
-        j -= 1
-    else:
-        break
-
-ans.append(abs(i-j-1))
-count = defaultdict(int)
-
-for c in arr:
-    count[c] += 1
-
-i = 0
-j = n - 1
-
-while True:
-    if check_rev(arr, count, j):
-        count[arr[j]] -= 1
-        j -= 1
-    else:
-        break
-
-while True:
-    if check(arr, count, i):
-        count[arr[i]] -= 1
-        i += 1
-    else:
-        break
-
-ans.append(abs(i-j-1))
-
-print(min(ans))
+c = len(set(arr))
+ans = float('inf')
+j = 0
+d = defaultdict(int)
+    
+for i in range(n):
+    while j < n:
+        if len(d) == c:
+            ans = min(ans, abs(i-j+1))
+            break
+        else:
+            d[arr[j]] += 1
+            j += 1
+    if len(d) == c:
+        ans = min(ans, abs(i-j+1))
+    d[arr[i]] -= 1
+    if not d[arr[i]]:
+        d.pop(arr[i])
+    
+print(ans+1)
+    
