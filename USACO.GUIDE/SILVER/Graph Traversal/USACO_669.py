d2a17eb4f68d8661b2f9a5eb419473cfe63b1dd9
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-07-10 01:39:45 +0600

    Update USACO_669.py

---
+++ USACO.GUIDE/SILVER/Graph Traversal/USACO_669.py
@@ -0,0 +1,52 @@
+from math import ceil, sqrt
+
+input = open('moocast.in', 'r').readline
+out = open('moocast.out', 'w')
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+
+
+## check
+def check(arr, dist):
+    #DFS
+    count = 1
+    stack = [0]
+    visited = [0]*len(arr)
+    visited[0] = 1
+    f = True
+    while stack:
+        i = stack.pop()
+        x1, y1 = arr[i]
+        for j in range(len(arr)):
+            x2, y2 = arr[j]
+            if not visited[j] and dist >= (x1-x2)**2 + (y1-y2)**2 :
+                stack.append(j)
+                visited[j] = 1
+                count += 1
+                f = False
+        if f:
+            return False
+
+    if count == len(arr):
+        return True
+    else:
+        return False
+
+
+hi = 25_000*25_000//2
+lo = 0
+while lo < hi:
+    mid = (hi+lo)//2
+    if check(arr, mid):
+        hi = mid
+    else:
+        lo = mid + 1
+
+    
+print(hi, file=out)
+# print(hi)
