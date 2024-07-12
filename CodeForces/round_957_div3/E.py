a978df1cf1f951bd79293d124caa15bc99da5fcd
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-07-12 23:41:01 +0600

    Update E.py

--- CodeForces/round_957_div3/E.py
+++ CodeForces/round_957_div3/E.py
@@ -1,31 +1,18 @@
 
-
-def ff(dig, i, n):
-    dig = list(str(dig))
-    n = list(str(n))
-    dig.extend(n[0:i%len(n)])
-    print(dig)
-    return int("".join(dig))
-
-
-def find_pairs(dig, n):
-    pairs = []
-    for b in range(1, 10_000+1):
-        if (dig+b)%n == 0:
-            pairs.append([(dig+b)//n, b])
-    # print(pairs)
-    return pairs
-
 def solve(n):
     pairs = []
-    dig = n
-    i = 1
-    while dig <= 10_000:
-        dig = ff(dig, i, n)
-        print(dig)
-        pairs.extend(find_pairs(dig, n))
-        i += 1
-    
+    nn = int("".join(str(n)*9))
+    # print(nn)
+    l = len(str(n))
+    while nn:
+        ll = len(str(nn))
+        for i in range(1, 10_000):
+            if (nn+i)%n == 0:
+                a = (nn+i)//n
+                if ll == (a*l-i):
+                    pairs.append([a, i])
+        nn = nn//10
+
     print(len(pairs))
     for a, b in pairs:
         print(a, b)
