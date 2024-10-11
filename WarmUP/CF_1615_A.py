23c1aad013794f9a6b48c1719b89c0e0ea018c17
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-10-11 23:21:03 +0600

    Create CF_1615_A.py

---
+++ WarmUP/CF_1615_A.py
@@ -0,0 +1,19 @@
+import gc
+# gc.disable()
+
+from math import ceil, floor, log
+
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    for i in range(ceil(log(max(arr), 2))*(n//2+1)):
+        arr.sort()
+        v = (arr[-1]-arr[0])//2
+        arr[0] += v
+        arr[-1] -= v
+        # print(arr)
+
+    arr.sort()
+    assert arr[-1] - arr[0] >= 0
+    print(arr[-1]-arr[0])
