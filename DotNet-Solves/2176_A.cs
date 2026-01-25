988c6938b2d5daa7a1a1b7b81eed1501c9dc0eb2
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2026-01-25 23:01:59 +0600

    Add solutions for problem 2176_A in C# and Python
    Added C# (2176_A.cs) and Python (2176_A.py) implementations for problem 2176_A. Updated .gitignore to exclude DotNet-Solves/.idea. Added output file placeholder in DotNet-Solves/out.

---
+++ DotNet-Solves/2176_A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+var _ = int.TryParse(Console.ReadLine(), out int test_cases);
+
+foreach (var __ in Enumerable.Range(0, test_cases))
+{
+    var n = int.Parse(Console.ReadLine());
+    var ans = 0;
+    var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    foreach (var i in Enumerable.Range(1, n-1))
+    {
+        ans += arr[i] > arr[0..i].Max() ? 0 : 1;
+    }
+    Console.WriteLine(ans);   
+}
---
+++ DotNet-Solves/2176_A.py
@@ -0,0 +1,10 @@
+﻿for _ in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    ans = 0
+    seg_size = 0
+    
+    for i in range(1, n):
+        ans += 0 if (arr[i] > max(arr[0: i])) else 1
+
+    print(ans)
