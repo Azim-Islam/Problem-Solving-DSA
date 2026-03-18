b523ec92f6fd2952c3b6c45a67d48445c9464fe6
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2026-03-18 13:48:14 +0600

    Update 2176_A.cs

--- DotNet-Solves/2176_A.cs
+++ DotNet-Solves/2176_A.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 
-var _ = int.TryParse(Console.ReadLine(), out int test_cases);
+var _ = int.TryParse(Console.ReadLine(), out int testCases);
 
-foreach (var __ in Enumerable.Range(0, test_cases))
+foreach (var __ in Enumerable.Range(0, testCases))
 {
-    var n = int.Parse(Console.ReadLine());
-    var ans = 0;
-    var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
-    foreach (var i in Enumerable.Range(1, n-1))
-    {
-        ans += arr[i] < arr[0..i].Max() ? 1 : 0;
-    }
+    var n = int.Parse(Console.ReadLine()!);
+    var arr = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    var ans = Enumerable.Range(1, n - 1).Sum(i => arr[i] < arr[0..i].Max() ? 1 : 0);
     Console.WriteLine(ans);   
 }
